abf550bc1f7a065d7956782a8bce59db65cf7f97
Elke Willems <willems.elke@gmail.com>
2026-09-14 15:04:26 +0200

    View en template toegevoegd voor takenlijst

--- taken/views.py
+++ taken/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Taak
+
+def takenlijst(request):
+    taken = Taak.objects.all()
+    return render(request, 'taken/takenlijst.html', {'taken': taken})
 
 # Create your views here.
